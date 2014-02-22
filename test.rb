f874cc11ca24584348c180141669194160cef3bd
animohan <anishmohan@gmail.com>
2014-02-22 07:03:22 -0800

    Some more lessons

--- test.rb
+++ test.rb
@@ -112,8 +112,90 @@ end
 A=[1,2,3,4,5]
 #numbers characters and string can be mixed in arrays
 B=['a',1,'cer','did']
+puts "Array B=[a,1,cer,did]"
+puts "B[0],B[1],B[2] are.."
 puts B[0]
 puts B[1]
 puts B[2]
 
+#Array indexes can be negative
+puts "B[-2],B[-1] are.."
 
+puts B[-2]
+puts B[-1]
+
+#Adding elements to arrays
+[1,2,3,4,5]<<"woot"
+[2,3,4,5,6].push('woot')
+
+
+#Transforming arrays
+puts "\n Transforming Arrays \n"
+Arr=[1,2,3,4,5]
+ puts A.map {|i| i+1}
+ puts A.map{|i| i*2}
+#note that map does not change the original content of the arrays
+
+#Filtering and Array
+puts "\n Filtering and Array\n"
+
+Arr=[2,3,4,5,6,7,8]
+puts "\n printing out even numbers \n"
+puts Arr.select{|number|number%2==0}
+
+puts "\n printing out odd number \n"
+puts Arr.select{|number|number%2!=0}
+
+game=['rock','paper','scissor','lizard','spock']
+puts game.select {|word| word.length>5}
+
+
+puts "Deleting elements"
+game.delete_if{|word| word.length<5}
+puts game
+
+
+# Back to loops
+puts "\n FOR LOOPS \n"
+
+for i in Arr
+	puts i
+end
+
+#Function to copy elements of array Arr into Brr if the number
+# is a multiple of 2
+
+def array_copy (source_arr)
+	newarr=[]
+	for i in source_arr # note how i is not the index here
+		if (i%2==0)
+			newarr<<i
+		end
+	end
+	puts "\n Even array is"
+	puts newarr
+end
+
+array_copy(Arr)
+
+puts "\nLooping with each\n"
+
+Brr=[22,23,24,25,26,27]
+Brr.each do |i|
+	puts i
+end
+
+def array_copy_each(source_arr)
+	newarr=[]
+	source_arr.each do |i|
+		if (i%2==0)
+			newarr<<i
+		end
+	end
+	puts "\n New even array now is.."
+	puts newarr
+
+end
+
+
+array_copy_each(Brr)
